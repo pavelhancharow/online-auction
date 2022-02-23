4982c8fcafa850b6b30597fc342055f43507795b
Pavel <pl.hancharou@gmail.com>
2022-02-23 17:09:52 +0300

    feat: create eslint config

---
+++ app/.eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+    'eslint-plugin-import',
+    'prettier',
+  ],
+  env: {
+    browser: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    project: ['tsconfig.json'],
+    ecmaVersion: 2020,
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.tsx', '.ts'],
+      },
+    ],
+    'react/prop-types': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        bracketSpacing: true,
+        tabWidth: 2,
+        useTabs: false,
+      },
+    ],
+    'max-len': ['warn', { code: 80 }],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
