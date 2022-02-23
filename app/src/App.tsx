77641626448d7323892fe310a8abdf364595cc49
Pavel <pl.hancharou@gmail.com>
2022-02-23 17:12:19 +0300

    feat: add a few components

---
+++ app/src/App.tsx
@@ -0,0 +1,6 @@
+import { FC } from 'react';
+import { Content } from './components/Content';
+
+export const App: FC = (): JSX.Element => {
+  return <Content />;
+};
---
+++ app/src/components/Content.tsx
@@ -0,0 +1,5 @@
+import { FC } from 'react';
+
+export const Content: FC = (): JSX.Element => {
+  return <div>Hello App!</div>;
+};
