fdfe9e33a931b62c3107dfd3ba00a3326846a163
Pavel <pl.hancharou@gmail.com>
2022-02-23 17:09:36 +0300

    feat: create webpack config

---
+++ app/webpack.config.ts
@@ -0,0 +1,85 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
+import Dotenv from 'dotenv-webpack';
+
+const webpackConfig = (env: any) => ({
+  entry: path.join(__dirname, '/src/index.tsx'),
+  ...(env.production || !env.development ? {} : { devtool: 'eval-source-map' }),
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    plugins: [new TsconfigPathsPlugin()],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: '[name].[fullhash].js',
+    assetModuleFilename: 'assets/[hash][ext]',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+        },
+        exclude: /dist/,
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          env.development ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          env.development ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        loader: 'webp-loader',
+      },
+    ],
+  },
+  plugins: [
+    new Dotenv(),
+    new HtmlWebpackPlugin({
+      title: env.development ? 'Development' : 'Online Auction App',
+      template: './public/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[fullhash].css',
+    }),
+    new webpack.DefinePlugin({
+      'process.env.PRODUCTION': env.production || !env.development,
+      'process.env.NAME': JSON.stringify(require('./package.json').name),
+      'process.env.VERSION': JSON.stringify(require('./package.json').version),
+    }),
+    new ForkTsCheckerWebpackPlugin({
+      eslint: {
+        files: './src/**/*.{ts,tsx,js,jsx}',
+      },
+    }),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+  ],
+});
+
+export default webpackConfig;
