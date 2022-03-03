7307118f7fcae57f9dae2c1525ebcdf1d91a7d41
Pavel <pl.hancharou@gmail.com>
2022-03-03 16:16:03 +0300

    feat: create form options data

---
+++ app/src/data/FormOptions.ts
@@ -0,0 +1,34 @@
+import { RegisterOptions } from 'react-hook-form';
+
+export const nameOptions: RegisterOptions = {
+  required: {
+    value: true,
+    message: 'This field is required',
+  },
+  pattern: {
+    value: /^[a-zA-Z][a-zA-Z ]{2,}$/,
+    message: 'Enter your name in Latin and avoid numbers',
+  },
+};
+
+export const emailOptions: RegisterOptions = {
+  required: {
+    value: true,
+    message: 'This field is required',
+  },
+  pattern: {
+    value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+    message: 'Enter a valid email',
+  },
+};
+
+export const passwordOptions: RegisterOptions = {
+  required: {
+    value: true,
+    message: 'This field is required',
+  },
+  minLength: {
+    value: 8,
+    message: 'The minimum password length is at least 8 characters.',
+  },
+};
