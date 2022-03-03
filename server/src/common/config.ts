83da1b072ad9f9e7b63b987430c70b5b124304b5
Pavel <pl.hancharou@gmail.com>
2022-03-03 16:37:39 +0300

    fix: add mongo url

--- server/src/common/config.ts
+++ server/src/common/config.ts
@@ -6,12 +6,15 @@ dotenv.config({ path: join(__dirname, '../../.env') });
 
 interface IConfig {
   PORT: string;
+  MONGO_URL: string;
 }
 
 enum Config {
   PORT = '3000',
+  MONGO_URL = '*****',
 }
 
 export default <IConfig>{
   PORT: env.PORT || Config.PORT,
+  MONGO_URL: env.MONGO_URL || Config.MONGO_URL,
 };
