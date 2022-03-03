4863c649a661d81eb84947b3cac57106b21ba643
Pavel <pl.hancharou@gmail.com>
2022-03-03 16:15:36 +0300

    feat: create form inputs data

---
+++ app/src/data/FormInputs.ts
@@ -0,0 +1,31 @@
+import { InputIdType, InputType } from 'src/models/InputTypes';
+
+interface IFormInput {
+  id: InputIdType;
+  type: InputType;
+  placeholder: string;
+}
+
+export const LoginFormInputs: IFormInput[] = [
+  { id: 'email', type: 'email', placeholder: 'E-mail address' },
+  { id: 'password', type: 'password', placeholder: 'Password' },
+];
+
+export const ResetFormInputs: IFormInput[] = [
+  {
+    id: 'email',
+    type: 'email',
+    placeholder: 'Enter e-mail address',
+  },
+];
+
+export const RegistFormInputs: IFormInput[] = [
+  { id: 'name', type: 'text', placeholder: 'Full name' },
+  { id: 'email', type: 'email', placeholder: 'E-mail address' },
+  { id: 'password', type: 'password', placeholder: 'Password' },
+  {
+    id: 'confirm',
+    type: 'password',
+    placeholder: 'Confirm password',
+  },
+];
