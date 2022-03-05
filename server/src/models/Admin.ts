d8007afa67dd4b0b3f4da9fda941011f9bb563af
Pavel <pl.hancharou@gmail.com>
2022-03-05 21:51:44 +0300

    refactor: remove old model

--- server/src/models/Admin.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from 'mongoose';
-
-const Admin = new Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  roles: [{ type: String, ref: 'Role' }],
-});
-
-export const AdminModel = model('Admin', Admin);
