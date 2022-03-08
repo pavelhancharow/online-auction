19ec0b4109cb7e64e9d52bffd561338d148baa32
Pavel <pl.hancharou@gmail.com>
2022-03-08 17:15:05 +0300

    feat: add auth middleware

--- server/src/resources/auth/authRouter.ts
+++ server/src/resources/auth/authRouter.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { getUsers, login, registration } from './authController';
+import { auth, getUsers, login, registration } from './authController';
 import { checkLogin, checkRegistration } from '../../services/checkRequests';
+import authMiddleware from '../../middleware/auth.middleware';
 
 export const authRouter = Router();
 
 authRouter.post('/registration', checkRegistration(), registration);
 authRouter.post('/login', checkLogin(), login);
 authRouter.get('/users', getUsers);
+authRouter.get('/auth', authMiddleware, auth);
