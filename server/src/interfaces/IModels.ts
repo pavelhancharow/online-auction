d45c726e64e78e0cae6afd29356f5884e7d21b74
Pavel <pl.hancharou@gmail.com>
2022-03-08 17:15:53 +0300

    fix: change user & role types

--- server/src/interfaces/IModels.ts
+++ server/src/interfaces/IModels.ts
@@ -1,10 +1,12 @@
-export interface IUser {
+import { Document } from 'mongoose';
+
+export interface IUser extends Document {
   username: string;
   email: string;
   password: string;
   roles: string[];
 }
 
-export interface IRole {
+export interface IRole extends Document {
   value: string;
 }
--- server/src/interfaces/UserType.ts
+++
@@ -1,5 +0,0 @@
-import { Document, Types } from 'mongoose';
-import { IUser } from './IModels';
-
-export type UserType = Document<unknown, IUser> &
-  IUser & { _id: Types.ObjectId };
