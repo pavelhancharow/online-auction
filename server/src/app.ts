d586c46bf3a1b88d0ae8dad4d55e583811c14b69
Pavel <pl.hancharou@gmail.com>
2022-03-08 17:09:54 +0300

    feat: add error middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { authRouter } from './resources/auth/authRouter';
 import { corsOptions } from './common/cors';
+import errorMiddleware from './middleware/error.middleware';
 
 export const app = express();
 
@@ -10,3 +11,5 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/auth', authRouter);
+
+app.use(errorMiddleware);
