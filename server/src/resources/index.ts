af11fa983a3b34aedb430e7d63b11a371e760304
Pavel <pl.hancharou@gmail.com>
2022-03-10 21:07:17 +0300

    refactor: combine routes

---
+++ server/src/resources/index.ts
@@ -0,0 +1,5 @@
+import { adminRouter } from './admin/adminRouter';
+import { auctionRouter } from './auction/auctionRouter';
+import { authRouter } from './auth/authRouter';
+
+export { adminRouter, auctionRouter, authRouter };
