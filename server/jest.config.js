0a0162a35c66a675766187b115b9b751a41d178a
Pavel <pl.hancharou@gmail.com>
2022-03-24 16:04:23 +0300

    feat: implement jest test

---
+++ server/jest.config.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line tsdoc/syntax
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/**/*.test.ts'],
+  verbose: true,
+  forceExit: true,
+};
